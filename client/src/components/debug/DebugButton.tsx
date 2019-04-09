5ba062cbad7829b008e0fa7c8dbf0f37ccdb8fe3
Robin Minervini <robin.minervini@gmail.com>
2019-04-09 17:30:37 +0200

    :sparkles: :lipstick: Improved debug panel
    - Renamed 'clients' folder to 'ApiClient'
    - Added sass
    - Added DebugStore
    - Added style to debug panel
    - Added button to show/hide debug panel

---
+++ client/src/components/debug/DebugButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ReactComponent as BugIcon } from '../../assets/images/bug.svg'
+import DebugStore from '../../store/DebugStore'
+
+const DebugButton = () => {
+	return (
+		<button className="debug-panel__button" onClick={DebugStore.toggleDebug}><BugIcon /></button>
+	)
+}
+
+export default DebugButton
