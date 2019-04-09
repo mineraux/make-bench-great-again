9903aac4c7928f8a6ba1a6178e3c4f27aa94b5b3
Unknown <robin.minervini@gmail.com>
2019-04-09 22:18:03 +0200

    Test mapbox

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import DebugPanel from './components/debug/DebugPanel';
+import ProtoMap from './components/ProtoMap'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <DebugPanel />
+      <ProtoMap />
       </div>
     );
   }
