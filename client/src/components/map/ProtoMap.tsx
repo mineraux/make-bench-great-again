f48aec6f811ce5d917a71cfa0f71e1cbfddf8015
Robin Minervini <robin.minervini@gmail.com>
2019-04-11 16:26:39 +0200

    - Added turf to dependencies to calculate distance - Get distance between userlocation and 2 markers - Added static benchlist array to store for development test (to avoid unecessary call to DB)

--- client/src/components/map/ProtoMap.tsx
+++ client/src/components/map/ProtoMap.tsx
@@ -1,26 +1,30 @@
-import React, { FunctionComponent, useEffect, useCallback, useRef } from "react";
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
 import './map.scss'
 import { observer } from "mobx-react-lite";
 import Store from "../../store/Store";
 import mapboxgl, { GeolocateControl, Map as MapboxGlMap } from 'mapbox-gl'
 // import MapBoxDirections from '@mapbox/mapbox-gl-directions'
 var MapboxDirections = require('@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions');
+import * as turf from '@turf/turf'
 
 const ProtoMap: FunctionComponent = () => {
 
-  const {benchList, fetchBenchList} = Store
+  const { benchList, fetchBenchList } = Store
 
   let map = useRef<MapboxGlMap | null>(null);
   let directions = useRef(null);
   let geolocate = useRef<GeolocateControl | null>(null)
 
-  const getInstallationList = async() => {
-    await fetchBenchList({name:true, description: true, geolocation: true})
-  } 
+  const [markers, setMarkers] = useState()
+  const [userLocation, setUserLocation] = useState()
+
+  const getInstallationList = async () => {
+    await fetchBenchList({ name: true, description: true, geolocation: true })
+  }
 
   useEffect(() => {
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN as string
-    
+
     map.current = new MapboxGlMap({
       container: 'map',
       zoom: 15,
@@ -30,54 +34,70 @@ const ProtoMap: FunctionComponent = () => {
 
     geolocate.current = new GeolocateControl({
       positionOptions: {
-          enableHighAccuracy: true
+        enableHighAccuracy: true
       },
       trackUserLocation: true
     })
 
-    geolocate.current.on('geolocate', function(e:any) {
+    geolocate.current.on('geolocate', function (e: any) {
+      setUserLocation([e.coords.longitude, e.coords.latitude])
       //@ts-ignore
       directions.current.setOrigin([e.coords.longitude, e.coords.latitude]);
       //@ts-ignore
       directions.current.setDestination([2.402, 48.8787])
     })
-    
-    map.current.on('load', function() {
+
+    map.current.on('load', function () {
       getInstallationList()
       if (geolocate.current) {
         geolocate.current.trigger()
       }
     })
 
-    
     map.current.addControl(geolocate.current);
 
-  //@ts-ignore
-  directions.current = new MapboxDirections({ 
-    accessToken: mapboxgl.accessToken,
-    unit: 'metric',
-    profile: 'mapbox/cycling'
-  });
-  //@ts-ignore
-  map.current.addControl(directions.current);
+    //@ts-ignore
+    directions.current = new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/cycling'
+    });
+    //@ts-ignore
+    map.current.addControl(directions.current);
   }, [])
 
   useEffect(() => {
-    if(map.current) {
+    const tempMarkers: mapboxgl.Marker[] = []
+    if (map.current) {
       benchList.map(bench => {
-        const marker = new mapboxgl.Marker().setLngLat(bench.geolocation).addTo(map.current!)
-        console.log(bench.geolocation)
+        let marker = new mapboxgl.Marker().setLngLat(bench.geolocation).addTo(map.current!)
+        tempMarkers.push(marker)
       })
+
+      setMarkers(tempMarkers)
     }
-  },[benchList])
-  const yolo = () => {
+  }, [benchList])
+
+  const loadInstalationList = () => {
     getInstallationList()
+  }
+
+  const getNearestMarker = () => {
+    console.log(userLocation)
+    markers.map((marker: mapboxgl.Marker) => {
+      const lngLat = marker.getLngLat()
+      const lngLatArr = [lngLat.lng, lngLat.lat]
+
+      const distance = turf.distance(userLocation, lngLatArr)
+      console.log(distance)
+    })
   }
 
   return (
     <>
       <div id="map"></div>
-      <button onClick={yolo}>YOLO</button>
+      <button onClick={loadInstalationList}>Load instalations data</button>
+      <button onClick={getNearestMarker}>Get nearest marker</button>
     </>
   )
 }
--- client/src/store/Store.ts
+++ client/src/store/Store.ts
@@ -6,9 +6,27 @@ class Store {
   @observable benchList:ApiBenchReponseRoot = []
 
   @action public fetchBenchList = async (fieldToFetch: QueryApiBenchReponse) => {
-    this.benchList = (await ApiClient.getBenchList(fieldToFetch)).map(
-      entry => ({... entry})
-    )
+    if (process.env.NODE_ENV === "development") {
+      this.benchList = [
+        {
+          name: "Bench1",
+          description: "Desc1",
+          lockedDescription: "Locked",       
+          geolocation: [2.402,48.8787]
+        },
+        {
+          name: "Bench2",
+          description: "Desc2",
+          lockedDescription: "Locked",
+          geolocation: [2.4059,48.8757]
+        }
+      ]      
+    } else {
+      this.benchList = (await ApiClient.getBenchList(fieldToFetch)).map(
+        entry => ({... entry})
+      )
+    }
+    
   }
 }
 
