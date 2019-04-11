2088a50081e929878e168fa454959de4e5814c46
Alexandre Massé <alexandre.masse94@gmail.com>
2019-04-11 18:27:06 +0200

    :cloud: :bird: get tweets with specific hastag

--- api/app.js
+++ api/app.js
@@ -4,12 +4,21 @@ const graphqlHttp = require('express-graphql')
 const mongoose = require('mongoose')
 const graphQLSchema = require('./graphql/schema/index.graphql')
 const graphQLResolvers = require('./graphql/resolvers/index')
+const twitter = require('twitter');
+const client = new twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+const PORT = 4000;
 
 const app = express()
 
-app.use(bodyParser.json())
+app.listen(PORT)
+console.log('Listening on port : ' + PORT)
 
-const PORT = 4000;
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -27,12 +36,25 @@ app.use('/api', graphqlHttp({
   graphiql: true
 }))
 
+
+app.get('/twitter', (req, res) => {
+
+  client.get('search/tweets', {q: "#apple", count: 1, result_type: "mixed"}, function (error, tweets, response) {
+    if (error) {
+      console.log("Error getting tweets");
+      return;
+    }
+    res.json(tweets);
+  });
+
+})
+
+
 mongoose
   .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
   .then(() => {
-    app.listen(PORT)
-    console.log('Listening on port : ' + PORT)
+    console.log('Sucess connect to mongoDB')
   })
   .catch(err => {
-    console.log(err)
+    console.error('Error connect to mongoDB', err)
   })
