8a56cf8f273d6cca1eab02989e3083e99bc8ffbd
Alexandre Massé <alexandre.masse94@gmail.com>
2019-04-24 01:35:28 +0200

    :cloud: :wrench: #8 API : now use dotenv for env variables

--- api/app.js
+++ api/app.js
@@ -1,22 +1,46 @@
+const process = require('process');
 const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
+
+// ENV 🔧
+
+const dotenv = require('dotenv')
+dotenv.config({path: __dirname + '/.env.local'})
+
+// DB 🗄
+
 const mongoose = require('mongoose')
 const graphQLSchema = require('./graphql/schema/index.graphql')
 const graphQLResolvers = require('./graphql/resolvers/index')
-const twitter = require('twitter');
-const client = new twitter({
+
+mongoose
+    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
+    .then(() => {
+      console.log('🗄 Success connect to mongoDB')
+    })
+    .catch(err => {
+      console.error('🗄 Error connect to mongoDB', err)
+    })
+
+// TWITTER 🐦
+
+const Twitter = require('twitter');
+const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+
+// APP 📱
+
 const PORT = 4000;
 
 const app = express()
 
 app.listen(PORT)
-console.log('Listening on port : ' + PORT)
+console.log('🚀 Listening on port : ' + PORT)
 
 app.use(bodyParser.json())
 
@@ -36,12 +60,11 @@ app.use('/api', graphqlHttp({
   graphiql: true
 }))
 
-
 app.post('/twitter/:hashtag', (req, res) => {
 
-  console.log("API /twitter/:hashtag");
-
   const hashtag = req.params.hashtag
+
+  console.log("🐦 API > /twitter/" + hashtag);
 
   client.get('search/tweets', {
     q: "#"+ hashtag,
@@ -50,22 +73,12 @@ app.post('/twitter/:hashtag', (req, res) => {
     lang: "fr"
   }, function (error, tweets, response) {
     if (error) {
-      console.log("Error getting tweets");
+      console.log("🐦 Error getting tweets");
     } else {
-      console.log("Success getting tweets");
+      console.log("🐦 Success getting tweets");
       res.json(tweets);
     }
 
   });
 
 })
-
-
-mongoose
-  .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
-  .then(() => {
-    console.log('Success connect to mongoDB')
-  })
-  .catch(err => {
-    console.error('Error connect to mongoDB', err)
-  })
