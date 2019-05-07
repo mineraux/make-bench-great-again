efbe8c82dd17a2dbf35c342ed217a22198e3f3af
Alexandre Massé <alexandre.masse94@gmail.com>
2019-05-07 19:49:22 +0200

    :sparkles: #46 add BurgerButton component

--- client/src/store/PageStore.ts
+++ client/src/store/PageStore.ts
@@ -2,18 +2,25 @@ import { action, observable } from 'mobx'
 
 type currentPagePathType = string | null
 type nextPagePathType = string | null
+type isMenuOpenType = boolean
 
 class PageStore {
   @observable currentPagePath: currentPagePathType = null
 
   @observable nextPagePath: nextPagePathType = null
 
-  @action public setCurrentPagePath = (value: currentPagePathType): void => {
+  @observable isMenuOpen = false
+
+  @action setCurrentPagePath = (value: currentPagePathType): void => {
     this.currentPagePath = value
   }
 
-  @action public setNextPagePath = (value: nextPagePathType): void => {
+  @action setNextPagePath = (value: nextPagePathType): void => {
     this.nextPagePath = value
+  }
+
+  @action setIsMenuOpen = (value: isMenuOpenType): void => {
+    this.isMenuOpen = value
   }
 }
 
