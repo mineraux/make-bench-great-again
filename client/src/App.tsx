6dd0774810aa57364bd10e0b63b0218b448dc7a6
Alexandre Massé <alexandre.masse94@gmail.com>
2019-05-15 02:01:04 +0200

    :lipstick: #98 improve header animation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -22,6 +22,7 @@ const App: FunctionComponent = () => {
     headerTitle,
     headerTheme,
     isMenuOpen,
+    setIsHeaderVisible,
   } = NavigationStore
 
   const windowHeight = useWindowSize().height
@@ -33,6 +34,10 @@ const App: FunctionComponent = () => {
           // init currentPagePath if is not set
           if (match && currentPagePath === null) {
             setCurrentPagePath(match.path)
+            // if is not home set header visible
+            if (match.path !== config.routes.Home.path) {
+              setIsHeaderVisible(true)
+            }
           }
           // If page match but is not current page in store : Queue page to nextPagePath
           if (match && currentPagePath !== match.path) {
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useRef } from 'react'
 import { observer } from 'mobx-react-lite'
 import { NavigationStore } from '../../store'
 import Classnames from 'classnames'
@@ -7,6 +7,7 @@ import BurgerButton, {
   themes as BurgerButtonThemes,
 } from '../BurgerButton/BurgerButton'
 import ProgressBar from '../ProgressBar/ProgressBar'
+import { TimelineMax, Power1 } from 'gsap'
 import './header.scss'
 
 export enum themes {
@@ -34,8 +35,21 @@ type Props = {
 const Header: FunctionComponent<Props> = ({ className, title, theme }) => {
   const { scrollProgression, isHeaderVisible } = NavigationStore
 
+  const ref = useRef<HTMLHeadElement>(null)
+  useEffect(() => {
+    if (ref.current && isHeaderVisible) {
+      const tl = new TimelineMax()
+
+      tl.to(ref.current, 1, {
+        transform: 'translateY(0)',
+        ease: Power1.easeInOut,
+      })
+    }
+  }, [isHeaderVisible])
+
   return (
     <header
+      ref={ref}
       className={Classnames(className, 'header', `theme-${theme}`, {
         visible: isHeaderVisible,
       })}
