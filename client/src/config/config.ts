92a6e39e5b6fc7b3f4b9ffa4d7e33b64ce583bd2
Alexandre Massé <alexandre.masse94@gmail.com>
2019-05-25 21:04:56 +0200

    :label: #136 add types to config.ts + add isMapButtonVisible on route config

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -7,55 +7,81 @@ import MenuTransition from '../pages/Menu/Transition'
 import InstallationTransition from '../pages/Installation/Transition'
 import FinishTransition from '../pages/Finish/Transition'
 
-export default {
+interface routeInterface {
+  path: string
+  name: string
+  component: any
+  inNav: boolean
+  isMapButtonVisible: boolean
+}
+
+interface routesInterface {
+  [k: string]: routeInterface
+}
+
+interface configInterface {
+  routes: routesInterface
+}
+
+const config: configInterface = {
   routes: {
     Home: {
       path: '/',
       name: 'Home',
       component: HomeTransition,
       inNav: true,
+      isMapButtonVisible: false,
     },
     Map: {
       path: '/map',
       name: 'Map',
       component: MapTransition,
       inNav: true,
+      isMapButtonVisible: false,
     },
     Twitter: {
       path: '/twitter',
       name: 'Twitter',
       component: TwitterTransition,
       inNav: true,
+      isMapButtonVisible: true,
     },
     Admin: {
       path: '/admin',
       name: 'Admin',
       component: AdminTransition,
       inNav: true,
+      isMapButtonVisible: true,
     },
     Components: {
       path: '/components',
       name: 'Component',
       component: ComponentsTransition,
       inNav: true,
+      isMapButtonVisible: true,
     },
     Menu: {
       path: '/menu',
       name: 'Menu',
       component: MenuTransition,
       inNav: true,
+      isMapButtonVisible: false,
     },
     Installation: {
       path: '/installation/:installationId',
       name: 'Installation',
       component: InstallationTransition,
       inNav: false,
+      isMapButtonVisible: true,
     },
     Finish: {
       path: '/finish',
       name: 'Finish',
       component: FinishTransition,
       inNav: false,
+      isMapButtonVisible: false,
     },
   },
 }
+
+export default config
