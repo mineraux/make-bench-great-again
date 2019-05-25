86dff8688e9e1778772399f995719a1049d1ac37
Unknown <robin.minervini@gmail.com>
2019-05-25 16:15:00 +0200

    :sparkles: #128 Updated schemas and method api due to slug added

--- api/models/installation.js
+++ api/models/installation.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const installationSchema = new Schema({   
+  slug: {
+    type: String,
+    required: true
+  },
   name: {
     type: String,
     required: true
