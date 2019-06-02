792771155d5408cdd1911c96f06e3e8a40b52ed3
Alexandre Massé <alexandre.masse94@gmail.com>
2019-06-02 21:20:16 +0200

    :construction: #132 Finish page : dynamic svg animation wip

--- client/src/pages/Finish/Finish.tsx
+++ client/src/pages/Finish/Finish.tsx
@@ -6,12 +6,101 @@ import { ReactComponent as Progress } from './progress.svg'
 // styles
 import './finish.scss'
 import { useWindowSize } from '../../utils/hooks'
+import { TweenMax, TimelineMax } from 'gsap'
 
 type Props = pageProps
 
 const Finish: FunctionComponent<Props> = ({ match }) => {
   const ref = useRef<HTMLDivElement>(null)
   const windowHeight = useWindowSize().height
+  const [strokeDashArray, setStrokeDashArray] = useState(0)
+
+  // const currentStep = 4;
+  // const totalStep = 5;
+
+  useEffect(() => {
+    if (ref.current) {
+      const path = ref.current.querySelector(
+        '.page-finish__svg #progressPath'
+      ) as SVGPathElement
+      const length = path.getTotalLength()
+      setStrokeDashArray(length)
+
+      const tl = new TimelineMax()
+      tl.fromTo(
+        path,
+        5,
+        {
+          strokeDashoffset: length - length / 6,
+        },
+        {
+          strokeDashoffset: 0,
+          delay: 1.5,
+        }
+      )
+    }
+  }, [])
+
+  const steps = [
+    { color: 'red' },
+    { color: 'green' },
+    { color: 'yellow' },
+    { color: 'purple' },
+    { color: '#ff9800' },
+    { color: 'yellow' },
+  ]
+
+  const renderSvg = () => (
+    <svg
+      overflow="visible"
+      viewBox="0 0 561.5 1071.5"
+      xmlns="http://www.w3.org/2000/svg"
+      className={'page-finish__svg'}
+    >
+      <path
+        id="motionPath"
+        d="m280.75.25c154.92 0 280.5 125.58 280.5 280.5v510c0 154.92-125.58 280.5-280.5 280.5-154.92 0-280.5-125.58-280.5-280.5v-510c0-154.92 125.58-280.5 280.5-280.5z"
+        fill="none"
+        stroke="#efcaca"
+        strokeMiterlimit="10"
+        strokeWidth=".5"
+      />
+      <path
+        id="progressPath"
+        d="m280.75.25c154.92 0 280.5 125.58 280.5 280.5v510c0 154.92-125.58 280.5-280.5 280.5-154.92 0-280.5-125.58-280.5-280.5v-510c0-154.92 125.58-280.5 280.5-280.5z"
+        fill="none"
+        stroke="red"
+        strokeMiterlimit="10"
+        strokeWidth="5"
+        strokeDasharray={strokeDashArray}
+      />
+      {steps.map((step, index) => (
+        <circle r="20" strokeWidth="0.5" key={index}>
+          <animateMotion
+            calcMode="linear"
+            dur="0s"
+            keyPoints={`${(1 / steps.length) * index};${(1 / steps.length) *
+              index}`}
+            keyTimes="0;1"
+            repeatCount={1}
+            fill="freeze"
+          >
+            <mpath xlinkHref="#motionPath" />
+          </animateMotion>
+          <animate
+            attributeType="CSS"
+            attributeName="opacity"
+            begin="0"
+            from="0"
+            to="1"
+            dur="1s"
+            repeatCount="1"
+            fill="freeze"
+          />
+        </circle>
+      ))}
+    </svg>
+  )
   return (
     <div
       className={'page-finish'}
@@ -20,7 +109,8 @@ const Finish: FunctionComponent<Props> = ({ match }) => {
         minHeight: windowHeight,
       }}
     >
-      <Progress className={'page-finish__svg'} />
+      {/*<Progress className={'page-finish__svg'} />*/}
+      {renderSvg()}
 
       <p className="page-finish__counter">01-05</p>
 
