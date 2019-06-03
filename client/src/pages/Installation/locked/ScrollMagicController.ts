35b531689394944fbbdec2ecd3d079e6250122a8
Unknown <robin.minervini@gmail.com>
2019-06-04 01:10:08 +0200

    :construction: :lipstick: #160 Added main animation on locked content
    Still need to fix scroll issue due to padding top

--- client/src/pages/Installation/locked/ScrollMagicController.ts
+++ client/src/pages/Installation/locked/ScrollMagicController.ts
@@ -1,4 +1,4 @@
-import { TimelineMax } from 'gsap'
+import { TimelineMax, Power2 } from 'gsap'
 //@ts-ignore
 import ScrollMagic from 'scrollmagic'
 import 'animation.gsap'
@@ -31,53 +31,89 @@ class ScrollMagicController {
       }
     )
 
-    const tweenPresentation = new TimelineMax().fromTo(
-      '.page-installation--locked__go-to-installation p',
-      0.5,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-      }
-    )
+    const tweenPresentation = new TimelineMax()
+      .add('blurPresentation')
 
-    const scenePart1Pin = new ScrollMagic.Scene({
-      duration: 1000,
-      triggerHook: 0,
-    })
-      .setPin('.page-installation--locked__presentation')
-      .addIndicators({ name: 'Pin' })
-      .setTween(tweenPresentationText)
-      .addTo(controller)
-    this.scenes.push(scenePart1Pin)
+      .fromTo(
+        '.page-installation--locked__presentation',
+        0.5,
+        {
+          filter: 'blur(0)',
+        },
+        {
+          autoRound: false,
+          filter: 'blur(12px)',
+          ease: Power2.easeInOut,
+        },
+        'blurPresentation'
+      )
+      .fromTo(
+        '.page-installation--locked__go-to-installation p',
+        0.5,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+        },
+        'blurPresentation+=0.1'
+      )
 
-    const scenePart2Pin = new ScrollMagic.Scene({
-      offset: 1340,
-      duration: 694.5,
-      triggerHook: 1,
-    })
-      .setPin('.page-installation--locked__go-to-installation')
-      .addIndicators({ name: 'Pin2' })
-      .setTween(tweenPresentation)
-      .addTo(controller)
-    this.scenes.push(scenePart2Pin)
+    const headerHeight = document
+      .querySelector('header')!
+      .getBoundingClientRect().height
+    const durationPart1 = 1000
+    const durationPart2 = 1000
+    console.log(headerHeight)
 
-    // SCENES
-    //const part1Height = 1000
     // const scenePart1Pin = new ScrollMagic.Scene({
-    //   duration: part1Height,
+    //   duration: durationPart1,
     //   triggerHook: 0,
     // })
-    //   // .setPin('.page-installation--locked')
-    //   // .addIndicators({ name: 'Pin' })
+    //   .setPin('.page-installation--locked__presentation')
+    //   .addIndicators({ name: 'Pin' })
     //   .setTween(tweenPresentationText)
     //   .addTo(controller)
     // this.scenes.push(scenePart1Pin)
 
-    // scenePage.on('progress', (event: any) => {
-    //   setScrollProgression(event.progress)
+    // const scenePart2Pin = new ScrollMagic.Scene({
+    //   offset: durationPart1,
+    //   duration: durationPart1 + durationPart2,
+    //   triggerHook: 0,
     // })
+    //   .setPin('.page-installation--locked__go-to-installation')
+    //   .addIndicators({ name: 'Pin2' })
+    //   .setTween(tweenPresentation)
+    //   .addTo(controller)
+    // this.scenes.push(scenePart2Pin)
+
+    const scenePart1Pin = new ScrollMagic.Scene({
+      duration: durationPart1 + durationPart2 + headerHeight,
+      triggerHook: 0,
+    })
+      .setPin('.page-installation--locked__wrapper')
+      .addIndicators({ name: 'Pin' })
+      .addTo(controller)
+    this.scenes.push(scenePart1Pin)
+
+    const scenePresentationText = new ScrollMagic.Scene({
+      duration: durationPart1 + headerHeight,
+      triggerHook: 0,
+    })
+      .setTween(tweenPresentationText)
+      .addIndicators({ name: 'Pin 2' })
+      .addTo(controller)
+    this.scenes.push(scenePresentationText)
+
+    const sceneTestimonyTextFade = new ScrollMagic.Scene({
+      duration: durationPart2 + headerHeight,
+      triggerHook: 0,
+      offset: durationPart1,
+    })
+      .setTween(tweenPresentation)
+      .addIndicators({ name: 'Pin 3' })
+      .addTo(controller)
+    this.scenes.push(sceneTestimonyTextFade)
   }
 
   public destroyScrollMagicScenes = () => {
