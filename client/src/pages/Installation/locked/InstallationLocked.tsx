196a58a30615c95c3aa74ab8538f0863d718c120
Robin Minervini <robin.minervini@gmail.com>
2019-06-04 12:23:13 +0200

    :lipstick: #160 Added redirection to calculate path to installation

--- client/src/pages/Installation/locked/InstallationLocked.tsx
+++ client/src/pages/Installation/locked/InstallationLocked.tsx
@@ -14,6 +14,9 @@ import {
   getHeaderHeight,
 } from '../../../utils/hooks'
 import { ReactComponent as Progress } from './progress.svg'
+import Button, {
+  themes as ButtonThemes,
+} from '../../../components/Button/Button'
 
 type Props = pageProps & {}
 
@@ -33,6 +36,11 @@ const InstallationLocked: FunctionComponent<Props> = ({ match, history }) => {
     }
     if (match && installation._id.length === 0) {
       getInstallationInformation()
+    }
+
+    return () => {
+      ScrollMagicController.destroyScrollMagicScenes()
+      window.scrollTo(0, 0)
     }
     // setIsMapButtonVisible(true)
   }, [])
@@ -56,6 +64,10 @@ const InstallationLocked: FunctionComponent<Props> = ({ match, history }) => {
   }
 
   const windowHeight = useWindowSize().height
+
+  const onButtonClick = () => {
+    console.log('test')
+  }
 
   return (
     <div className="page-installation--locked">
@@ -113,6 +125,14 @@ const InstallationLocked: FunctionComponent<Props> = ({ match, history }) => {
           }}
         >
           <p>Rendez vous devant l'oeuvre pour débloquer le contenu</p>
+
+          <Button
+            onClick={onButtonClick}
+            label={'Calculer mon itinéraire'}
+            theme={ButtonThemes.Green}
+            link={'/map'}
+            className={'informations-panel__set-direction-button'}
+          />
         </div>
       </div>
     </div>
