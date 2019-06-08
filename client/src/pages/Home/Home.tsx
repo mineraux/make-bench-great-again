10e19dbf052bd8cb265901a6089f95cfefa8f531
Alexandre Massé <alexandre.masse94@gmail.com>
2019-06-08 19:31:53 +0200

    :fire: Home : remove useless code

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,36 +1,23 @@
-import React, {
-  FunctionComponent,
-  Fragment,
-  useEffect,
-  useState,
-  useRef,
-} from 'react'
+import React, { FunctionComponent, useEffect, useState, useRef } from 'react'
 import { pageProps } from '../types'
 import config from '../../config/config'
 import Button, { themes as buttonThemes } from '../../components/Button/Button'
-import { useScrollSpeed, useWindowSize, useClientRect } from '../../utils/hooks'
-import { getHeaderHeight } from '../../utils'
+import { useWindowSize } from '../../utils/hooks'
 import ScrollMagicController from './ScrollMagicController'
-
 import SplashscreenAnimation from '../../components/SplashscreenAnimation/SplashscreenAnimation'
 import { NavigationStore } from '../../store'
-import { TimelineMax, TweenMax, Power1, Power2 } from 'gsap'
+import { TimelineMax, Power1, Power2 } from 'gsap'
 // styles
 import './home.scss'
 
 type Props = pageProps
 
 const Home: FunctionComponent<Props> = ({ match }) => {
-  const scrollSpeed = useScrollSpeed()
   const [isSplashscreenCompleted, setIsSplashscreenCompleted] = useState(false)
-  const [isTextReady, setIsTextReady] = useState(false)
   const ref = useRef<HTMLDivElement>(null)
   const { setIsHeaderVisible } = NavigationStore
   const windowHeight = useWindowSize().height
   const refContainer = useRef<HTMLDivElement>(null)
-
-  let xDown: number
-  let yDown: number
 
   useEffect(() => {
     setIsHeaderVisible(false)
@@ -43,36 +30,7 @@ const Home: FunctionComponent<Props> = ({ match }) => {
     if (isSplashscreenCompleted) {
       ScrollMagicController.initController()
       if (ref.current) {
-        const containers = ref.current.querySelectorAll(
-          '.page-home__containers-wrapper__title, .page-home__containers-wrapper__container-1, .page-home__containers-wrapper__container-2'
-        )
-
-        const tl = new TimelineMax({
-          onComplete: () => {
-            setIsTextReady(true)
-          },
-        })
-
-        // tl.to(
-        //   containers,
-        //   1.5,
-        //   {
-        // filter: 'blur(0)',
-        // autoRound: false,
-        // ease: Power2.easeOut,
-        //   },
-        //   0
-        // ).to(
-        //   [
-        //     '.page-home__containers-wrapper__title',
-        //     '.page-home__containers-wrapper__container-1',
-        //   ],
-        //   1,
-        //   {
-        //     opacity: 1,
-        //   },
-        //   0
-        // )
+        const tl = new TimelineMax()
 
         tl.fromTo(
           '.page-home__containers-wrapper',
@@ -89,87 +47,15 @@ const Home: FunctionComponent<Props> = ({ match }) => {
           }
         )
       }
-      // if (ref.current) {
-      //   ref.current.addEventListener('touchstart', handleTouchStart, false)
-      //   ref.current.addEventListener('touchmove', handleTouchMove, false)
-      // }
     }
   }, [isSplashscreenCompleted])
 
-  const getTouches = (evt: TouchEvent) => {
-    return evt.touches
-  }
-
-  const handleTouchStart = (evt: TouchEvent) => {
-    const firstTouch = getTouches(evt)[0]
-    xDown = firstTouch.clientX
-    yDown = firstTouch.clientY
-  }
-
-  const handleTouchMove = (evt: TouchEvent) => {
-    if (!xDown || !yDown) {
-      return
-    }
-
-    let xUp = evt.touches[0].clientX
-    let yUp = evt.touches[0].clientY
-
-    let xDiff = xDown - xUp
-    let yDiff = yDown - yUp
-
-    if (Math.abs(xDiff) > Math.abs(yDiff)) {
-      /*most significant*/
-      if (xDiff > 0) {
-        /* left swipe */
-      } else {
-        /* right swipe */
-      }
-    } else {
-      const animationDuration = 1.5
-      if (ref.current) {
-        // const containers = ref.current.querySelectorAll(
-        //   '.page-home__containers-wrapper__container-1, .page-home__containers-wrapper__container-2'
-        // )
-
-        if (ref && ref.current) {
-          // TweenMax.to('.page-home__containers-wrapper', animationDuration / 2, {
-          //   autoRound: false,
-          //   filter: `blur(1px)`,
-          //   onComplete: () => {
-          //     TweenMax.to(
-          //       '.page-home__containers-wrapper',
-          //       animationDuration / 2,
-          //       {
-          //         autoRound: false,
-          //         filter: `blur(0)`,
-          //       }
-          //     )
-          //   },
-          // })
-        }
-      }
-
-      if (yDiff > 0) {
-        /* up swipe */
-        TweenMax.to('.page-home__containers-wrapper', animationDuration, {
-          y: -refContainer.current!.clientHeight,
-        })
-      } else {
-        /* down swipe */
-        // window.scrollTo(0, 0) // for safari top bar
-        TweenMax.to('.page-home__containers-wrapper', animationDuration, {
-          y: 0,
-        })
-      }
-    }
-
-    /* reset values */
-    xDown
-    yDown
-  }
-
   const handleSplashscreenComplete = () => {
     setIsSplashscreenCompleted(true)
+  }
+
+  const handleOnClickButton = () => {
+    setIsHeaderVisible(true)
   }
 
   return (
@@ -224,7 +110,7 @@ const Home: FunctionComponent<Props> = ({ match }) => {
               label={'Commencer'}
               theme={buttonThemes.Green}
               link={config.routes.Map.path}
-              onClick={() => setIsHeaderVisible(true)}
+              onClick={handleOnClickButton}
             />
           </div>
         </div>
